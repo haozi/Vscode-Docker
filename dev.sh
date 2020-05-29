0b5b7ab769030647c3fcf9300278af687c275c73
haozi <syntaxright@gmail.com>
2020-05-29 12:01:37 +0800

    feat: 🎸 change vscode-icons to Material Icon Theme

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 docker run --name='vscode' --hostname='vscode' \
 -v $PWD/data/config:/root/.local/share/code-server \
--v $PWD/data/config/extensions/vscode-icons-team.vscode-icons-9.7.0:/workspace \
+-v $PWD/data/config/extensions/octref.vetur-0.23.0:/workspace \
 -it -p 8080:8080 --rm haozhi/vscode
